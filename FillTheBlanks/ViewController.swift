bc1e180cd0e56d3ae0ed0ace7639d1a923c3584e
Sudhanshu Sudhanshu <sudhanshu@tillt04133.local>
2018-09-28 19:19:38 +0530

    More

--- FillTheBlanks/ViewController.swift
+++ FillTheBlanks/ViewController.swift
@@ -16,8 +16,12 @@ class ViewController: UIViewController {
     var blankDict: [String: Any] = [:] // To keep track of location and range of blank fields
     var mutableRanges: [NSRange] = []
     var changingRange: NSRange?
+    
+    var answers: [String] = []
+    
     let textView: LWTextView = {
         let textView = LWTextView()
+        textView.font = UIFont.systemFont(ofSize: 16)
         textView.text = "I thought it was important to \(blankSpaceHolder) but now I think its more \(blankSpaceHolder) important to 📷"
         textView.backgroundColor = .red
         return textView
@@ -92,8 +96,13 @@ class ViewController: UIViewController {
         setupKeyBoardNotification()
         
         view.addGestureRecognizer(UITapGestureRecognizer.init(target: self, action: #selector(tapGHandler)))
+        updateBlankRange()
+    }
+    
+    
+    func updateBlankRange() {
         let textRange = NSMakeRange(0, textView.text.count)
-        let expression = try! NSRegularExpression(pattern: "\(blankTag)", options: NSRegularExpression.Options())
+        let expression = try! NSRegularExpression(pattern: "\(blankSpaceHolder)", options: NSRegularExpression.Options())
         let array = expression.matches(in: textView.text, options: .reportProgress, range: textRange)
         for res in array {
             let range = res.range
@@ -106,12 +115,13 @@ class ViewController: UIViewController {
             let attributes = [
                 .foregroundColor: foregroundColor,
                 .backgroundColor: backgroundColor,
-                .font:  UIFont.systemFont(ofSize: 17),
+                .font:  UIFont.boldSystemFont(ofSize: 17),
                 NSAttributedStringKey.init(rawValue: "blank"): "blank"
                 ] as [NSAttributedStringKey : Any]
             
             textView.textStorage.addAttributes(attributes, range: range)
         }
+        textView.mutableRanges = mutableRanges
     }
     
 //    func allBlankFields() -> [NSRange] {
@@ -141,21 +151,85 @@ extension ViewController: UITextViewDelegate {
 //            return true
 //        }
         
-        for range in mutableRanges {
-            let changingRange = textView.selectedRange
-            print("changingRange: \(changingRange)")
-            let overlap = NSIntersectionRange(changingRange, range)
-            print("overlap: \(overlap)")
-            if /*overlap.location != 0 || */overlap.length != 0 {
-                return true // the ranges overlap
+        let cursorPosition = textView.selectedRange
+
+//        for mutableRange in mutableRanges {
+        let tempRangeArr = mutableRanges
+        for i in 0 ..< tempRangeArr.count {
+            var mutableRange = mutableRanges[i]
+            if cursorPosition.location >= mutableRange.location /*&& cursorPosition.location < (mutableRange.location + mutableRange.length)*/ {
+                print("Should be editing in range")
+                print("cursorPosition: \(cursorPosition)")
+               
+                if let textRange = range.toTextRange(textInput: textView) {
+                    
+                    if text.isEmpty {
+                        textView.replace(textRange, withText: text)
+                        mutableRange.length = mutableRange.length - range.length
+                        if mutableRange.length <= 0 {
+                            textView.replace(textRange, withText: blankSpaceHolder)
+                        }
+                        mutableRanges[i] = mutableRange
+                    }else {
+                        textView.replace(textRange, withText: text)
+                        let text = textView.text(in: textRange)
+                        if text == blankSpaceHolder {
+                            mutableRange.length =  range.length
+                        }else {
+                            mutableRange.length =  mutableRange.length + range.length
+                        }
+                        
+                        mutableRanges[i] = mutableRange
+                    }
+                    
+                }
+                
+                
+                updateBlankRange()
+                return false
+            }else {
+                continue
+//                textView.isEditable = false
+//                textView.resignFirstResponder()
+//                return false
             }
+            
+            
+//            let changingRange = textView.selectedRange
+//            print("changingRange: \(changingRange)")
+//            let overlap = NSIntersectionRange(changingRange, range)
+//            print("overlap: \(overlap)")
+//            if /*overlap.location != 0 || */overlap.length != 0 {
+//                return true // the ranges overlap
+//            }
         }
         return false
     }
 }
-
+extension NSRange {
+    func toTextRange(textInput:UITextInput) -> UITextRange? {
+        
+        if let rangeStart = textInput.position(from: textInput.beginningOfDocument, offset: location), let rangeEnd = textInput.position(from: rangeStart, offset: length){
+            return textInput.textRange(from: rangeStart, to: rangeEnd)
+        }
+        return nil
+    }
+}
 
 class LWTextView: UITextView  {
+    
+    var mutableRanges: [NSRange] = []
+    
+    override func addGestureRecognizer(_ gestureRecognizer: UIGestureRecognizer) {
+        if gestureRecognizer.isKind(of: UILongPressGestureRecognizer.self) {
+          return
+        }
+        super.addGestureRecognizer(gestureRecognizer)
+    }
+    
+    override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
+        return false
+    }
     
     override init(frame: CGRect, textContainer: NSTextContainer?) {
         super.init(frame: frame, textContainer: textContainer)
@@ -181,10 +255,28 @@ class LWTextView: UITextView  {
         // find the character that's been tapped
         let characterIndex = self.layoutManager.characterIndex(for: location, in: self.textContainer, fractionOfDistanceBetweenInsertionPoints: nil)
         if characterIndex < self.textStorage.length {
-            // if the character is a link, handle the tap as UITextView normally would
-            if (self.textStorage.attribute(NSAttributedStringKey(rawValue: "blank"), at: characterIndex, effectiveRange: nil) != nil) {
+            
+//            if (self.textStorage.attribute(NSAttributedStringKey(rawValue: "blank"), at: characterIndex, effectiveRange: nil) != nil) {
+            if (self.textStorage.attribute(.backgroundColor, at: characterIndex, effectiveRange: nil) != nil) {
+
                 self.isEditable = true
                 self.becomeFirstResponder()
+                
+                for mutableRange in mutableRanges {
+                    
+                    if characterIndex >= mutableRange.location && characterIndex <= (mutableRange.location + mutableRange.length) {
+                        self.selectedRange = mutableRange
+//                        return true
+                    }else {
+//                        textView.isEditable = false
+//                        textView.resignFirstResponder()
+//                        return false
+                    }
+                }
+                
+                
+                
+                
                 return self
             }
         }
